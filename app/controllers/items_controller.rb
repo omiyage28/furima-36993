371f1321f6a5ebdff50718a94110960b0db8d7b9
omiyage28 <nwsyt282828@gmail.com>
2021-10-13 20:13:49 +0900

    商品出品機能⑥　未ログイン時にログインPへ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
     def index
       @items = Item.all
     end
@@ -24,6 +25,10 @@ class ItemsController < ApplicationController
       :category_id, :status_id, 
       :shipping_method_id, :prefecture_id,
       :shipping_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
   end
  
   end
